bdbc3ce9252d5c2f9191515d54965630eb365a93
pien <pincenna@gmail.com>
2021-04-15 13:00:26 +0700

    satu - percobaan sebelas (bug)

--- satu/percobaan/sebelas.cpp
+++ satu/percobaan/sebelas.cpp
@@ -1,32 +1,43 @@
-#include <stdio.h>
-#include <string.h>
-
-struct dtnilai
-{
-    char nim[10];
-    char nama[20];
-    double nilai;
-};
-
-struct dtnilai data[10];
-int j = 0;
-
-int tambah_data()
-{
-    char jawab[2];
-    while (1)
-    {
-        fflush(stdin);
-        printf("NIM\t:");
-        scanf("%s", &data[j].nim);
-
-        printf("Nama\t:");
-        scanf("%s", &data[j].nama);
-
-        printf("Nilai test\t:");
-        scanf("%lf", &data[j].nilai);
-
-        printf("Ada data lagi? [y/t]\t:");
-    }
-    
+#include <stdio.h> 
+#include <string.h> 
+struct dtnilai 
+{ 
+char nrp[10];  
+char nama[20];  
+double nilai; 
+}; 
+struct dtnilai data[10]; 
+int j=0; 
+void tambah_data() 
+{ 
+char jawab[2]; 
+while(1) 
+{ 
+fflush(stdin); 
+printf("NRP :");scanf("%s",&data[j].nrp);  
+printf("Nama :");scanf("%s",&data[j].nama);  
+printf("Nilai Test :");scanf("%lf",&data[j].nilai); 
+printf("Ada data lagi(y/t):"); scanf("%s",&jawab); 
+if((strcmp(jawab,"Y")==0)||(strcmp(jawab,"y")==0)) 
+{ 
+j++;continue; 
+} 
+else if ((strcmp(jawab,"T")==0)||(strcmp(jawab,"t")==0)) break; 
+} 
+} 
+void tampil() 
+{ 
+int i; 
+printf("Data Mahasiswa yang telah diinputkan :\n"); 
+printf("NRP\tNama\tNilai\n"); 
+for (i=0;i<=j;i++)
+{ 
+printf("%s\t%s\t%6.2f\n",data[i].nrp,data[i].nama, 
+data[i].nilai); 
+} 
+} 
+int main() 
+{ 
+tambah_data(); 
+tampil(); 
 }
