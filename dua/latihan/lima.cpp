91ac1780309aa34cd8efb5a576b68c26241b8008
pien <pincenna@gmail.com>
2021-05-09 16:46:21 +0700

    dua - latihan lima

--- dua/latihan/lima.cpp
+++ dua/latihan/lima.cpp
@@ -18,10 +18,12 @@ void Push(ItemType, Stack *);
 ItemType Pop(Stack *);
 int drjt(char);
 void konversi_cetak(char []);
+int cek(char []);
 
 int main(int argc, char const *argv[])
 {
     char tampung[MAXSTACK], jawab;
+    int check;
 
     puts("MENGUBAH NOTASI INFIX MENJADI POSTFIX");
     puts("DENGAN MEMANFAATKAN STRUKTUR STACK");
@@ -32,9 +34,13 @@ int main(int argc, char const *argv[])
         puts("");
         printf("Masukkan ekspresi dalam notasi infix : ");
         fgets(tampung, sizeof(tampung), stdin);
-        printf("Ungkapan postfixnya = ");
-        konversi_cetak(tampung);
-        puts("");
+        check = cek(tampung);
+        if (check == 0)
+        {
+            printf("Ungkapan postfixnya = ");
+            konversi_cetak(tampung);
+            puts("");
+        }
         fflush(stdin);
         printf("\nMau mencoba lagi (y/t) ? ");
         scanf("%c", &jawab);
@@ -99,6 +105,7 @@ int drjt(char x)
     else
         return -1;
 }
+
 void konversi_cetak(char temp[])
 {
     int i, pjg, valid = 1;
@@ -153,4 +160,103 @@ void konversi_cetak(char temp[])
         smtr = Pop(&tumpukan);
         printf("%c", smtr);
     }
+}
+
+int cek(char temp[])
+{
+    int i, pjg, kurung = 1, operatr = 1, operand = 0, x;
+    char kar, smtr;
+
+    // printf("MULAI CEK\n");
+
+    pjg = strlen(temp) - 1;
+    for (i = 0; i < pjg; i++)
+    {
+        kar = temp[i];
+        switch (kar)
+        {
+        case '(':
+            operand = 0;
+            kurung = 0;
+            if (operatr == 0)
+            {
+                operatr = 2;
+            }
+            break;
+        
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+            if (operand == 0)
+            {
+                operand = 1;
+            }
+            if (operatr != 2)
+            {
+                operatr = 0;
+            }
+            
+            break;
+
+        case '+': case '-': case '*': case '/': case '^':
+            if (operand == 0 || operand == 2)
+            {
+                operand = 2;
+                break;
+            }
+            operand = 0;
+            if (operatr == 0)
+            {
+                operatr = 1;
+            }
+            
+            break;
+            
+        
+        case ')':
+            if (kurung == 0)
+            {
+                kurung = 1;
+            } else if (kurung == 1)
+            {
+                kurung = 0;
+            }
+
+            if (operand == 0 || operand == 2)
+            {
+                operand = 2;
+                break;
+            }
+            
+            break;
+            
+            
+        }
+
+        // printf("saat char nya '%c', operand nya '%d', operator nya '%d', dan kurungnya '%d'\n", kar, operand, operatr, kurung);
+    }
+
+    // printf("kurungnya '%d'", kurung);
+
+    x = 0;
+
+    if (operatr == 2)
+    {
+        printf("Output : Notasi infix salah, operator tidak lengkap\n");
+        x = 1;
+    }
+    
+    if (operand == 2)
+    {
+        printf("Output : Notasi infix salah, operand tidak lengkap\n");
+        x = 2;
+    }
+
+    if (kurung == 0)
+    {
+        printf("Output : Notasi infix salah, kurung tidak lengkap\n");
+        x = 3;
+    }
+
+    // printf("SELESAI CEK\n");
+
+    return x;
 }
