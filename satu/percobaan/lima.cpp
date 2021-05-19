6078b7b61d5ad226b4415d3729f350db81697339
pien <pincenna@gmail.com>
2021-05-19 20:06:03 +0700

    satu - percobaan lima

--- satu/percobaan/lima.cpp
+++ satu/percobaan/lima.cpp
@@ -12,8 +12,8 @@ int main(int argc, char const *argv[])
 
     printf("Alamat x = %p\n", &x);
     printf("Isi px = %p\n", px);
-    printf("Isi x = %p\n", x);
-    printf("Nilai yang ditunjuk oleh px = %p\n", *px);
-    printf("Nilai y = %p\n", y);
+    printf("Isi x = %d\n", x);
+    printf("Nilai yang ditunjuk oleh px = %d\n", *px);
+    printf("Nilai y = %d\n", y);
     return 0;
 }
